0a5dc72c077ffbe2712b31fa715d71d61699eedd
Nilesh Agrawal <nilesh.d.agrawal@gmail.com>
2015-10-30 02:47:22 -0700

    -- Adding switcher. -- Adding Textfield -- Adding Marked and Unmarked Questions.

--- PunchIn/StudentCourseDraggableViewController.swift
+++ PunchIn/StudentCourseDraggableViewController.swift
@@ -267,9 +267,7 @@ class StudentCourseDraggableViewController: UIViewController {
             
         }
     }
-    
 
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
