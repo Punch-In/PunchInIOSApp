6264bfd183a0673e71a87a6766603b6cbf840523
Nilesh Agrawal <nilesh.d.agrawal@gmail.com>
2015-10-31 18:12:14 -0700

    Adding the storyboard some images and some collection view cell files

---
+++ PunchIn/AttendanceDisplayCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AttendanceDisplayCollectionViewCell.swift
+//  PunchIn
+//
+//  Created by Nilesh Agrawal on 10/31/15.
+//  Copyright © 2015 Nilesh Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class AttendanceDisplayCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ PunchIn/ClassNameCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ClassNameCollectionViewCell.swift
+//  PunchIn
+//
+//  Created by Nilesh Agrawal on 10/31/15.
+//  Copyright © 2015 Nilesh Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ClassNameCollectionViewCell: UICollectionViewCell {
+    
+}
